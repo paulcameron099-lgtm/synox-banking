a350ae648085c715ee61c8fc85946fa1ec2a5cf1
steeve7 <stephenchidindu12@gmail.com>
2026-05-16 19:09:51 +0100

    done

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
                   Write to us
                 </h3>
                 <p className="text-white font-medium font-Poppins xl:text-[20px] lg:text-[18px] md:text-[16px] text-[14px]">
-                  office@synox.com
+                  support@synox.com
                 </p>
               </div>
             </div>
